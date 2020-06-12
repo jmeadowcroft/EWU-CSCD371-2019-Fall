05305a81ace073f792e51c33a794fec1812fbe27
Jenny Meadowcroft <jenny.meadowcroft@IntelliTect.com>
2020-06-12 15:16:15 -0700

    WIP

--- Logger.Tests/MakeATitleTests.cs
+++ Logger.Tests/MakeATitleTests.cs
@@ -5,7 +5,15 @@ namespace MakeATitle.tests
     [TestClass]
     class MakeATitleTests
     {
+        
         [TestMethod]
-        public void
+        public void ToTitleCase_UpperAndLower_ReturnsCorrectly()
+        {
+            string myString = "intellitect is a company in Spokane";
+            string myTitle = myString.ToTitleCase();
+            Assert.AreEqual(myTitle, "intellitect is a company in Spokane");
+        }
+
+        
     }
 }
